a06d20818700a5e80b1b55c356069727e31b9ca1
Nikolai Perlt <nikolaiperlt@gmail.com>
2018-11-25 17:04:37 +0100

    travis test

---
+++ CarondoProxy/src/test/java/facade/ProxyFacadeTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import dto.CarDTO;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author perlt
+ */
+public class ProxyFacadeTest {
+    
+    
+    /**
+     * Test of getCars method, of class ProxyFacade.
+     */
+    @Test
+    public void testGetCars() throws Exception {
+        System.out.println("getCars");
+        String color = "black";
+        String eco = "all";
+        int minSize = 1;
+        int maxSize = 4;
+        int minPrice = 1;
+        int maxPrice = 3000000;
+        ProxyFacade instance = new ProxyFacade();
+        List<CarDTO> result = instance.getCars(color, eco, minSize, maxSize, minPrice, maxPrice);
+        assertTrue(!result.isEmpty());
+    }
+    
+}
