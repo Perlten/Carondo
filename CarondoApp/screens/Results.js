a5e9df79ec62764f9e70daf60e888b7918ec5e9f
Nikolai Perlt <nikolaiperlt@gmail.com>
2018-11-28 17:39:03 +0100

    merge master

---
+++ CarondoApp/screens/Results.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+export default class Results extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+        <View>
+            <Text>Hot damn we made it</Text>
+        </View>
+        );
+    }
+}
