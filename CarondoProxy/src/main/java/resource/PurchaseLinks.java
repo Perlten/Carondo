1db37ef9d6dc1824d925f5bdc0fb6bb25e0568d9
adamlass <33089759+adamlass@users.noreply.github.com>
2018-12-04 10:22:30 +0100

    methods

---
+++ CarondoProxy/src/main/java/resource/PurchaseLinks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resource;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author adamlass
+ */
+public class PurchaseLinks {
+    private static HashMap<String,String> links = new HashMap<>();
+
+    
+    public static String getLink(String hash){
+       
+    }
+    
+    public static void saveLink(String link){
+        
+    }
+    
+}
