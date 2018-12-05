ec2b26d9b1409a045223c7721dad1df0e6c395ce
JesperRusbjerg <cph-jr211@cphbusiness.dk>
2018-12-05 12:52:37 +0100

    fix

--- carondo_employee/src/facade/URL.js
+++ carondo_employee/src/facade/URL.js
@@ -1,4 +1,3 @@
-// const URL = "https://perlt.net/Carondo/api/"
-const URL = "http://localhost:8084/CarondoProxy/api/"
+ const URL = "https://perlt.net/Carondo/api/"
 // const URL = "https://2943a0a8.ngrok.io/CarondoProxy/api/"
 export default URL;
