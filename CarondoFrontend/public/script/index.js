176eb72676d624059ce7fe8b4908a60dee317d0b
adamlass <33089759+adamlass@users.noreply.github.com>
2018-12-05 17:58:33 +0100

    adding frontend one more time

---
+++ CarondoFrontend/public/script/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Home from './components/Home';
+import Result from './components/Result';
+let router =  (
+    <Router history={browserHistory}>
+        <Route path = '/' component={Home}></Route>
+        <Route path = '/result' component={Result}></Route>
+    </Router>
+);
+
+ReactDOM.render(
+    router,
+    document.getElementById('app')
+);;
